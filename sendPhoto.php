16ddfef596e64f1ac03d229cecbe097eaeff7bcd
jarriztg <jarriztg@users.noreply.github.com>
2016-06-01 02:36:22 -0500

    Update sendPhoto.php

--- sendPhoto.php
+++ sendPhoto.php
@@ -7,7 +7,7 @@ $website = "https://api.telegram.org/bot".$botToken;
 
 <form action="<?php echo $website.'/sendPhoto' ?>" methos="post" enctype"multipart/form-data">
 
-	<input type="text" name="chat_id" value="189041244" />
+	<input type="text" name="chat_id" value="" />
 	<input type="file" name="photo" />
 	<input type="submit" value="Enviar" />
 
