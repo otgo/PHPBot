f04e2b64cfa861d5e25969754bde79be7a3eb8ca
jarriztg <jarriztg@users.noreply.github.com>
2016-06-01 02:35:24 -0500

    Create sendPhoto.php

---
+++ sendPhoto.php
@@ -0,0 +1,14 @@
+<?php
+
+$botToken = "189074074:AAH2PmSI_TOfmfFzWa7uzkUlcc0QU5W5X84";
+$website = "https://api.telegram.org/bot".$botToken;
+
+?>
+
+<form action="<?php echo $website.'/sendPhoto' ?>" methos="post" enctype"multipart/form-data">
+
+	<input type="text" name="chat_id" value="189041244" />
+	<input type="file" name="photo" />
+	<input type="submit" value="send" />
+
+</form>
